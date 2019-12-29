c8dd386fefe8b171fe08288d3e7d7a59300ca5eb
Tikaram Mardi <tikarammardi007@gmail.com>
2019-12-29 14:51:11 +0530

    auth added

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 const validator = require('validator');
-const User = mongoose.model('User', {
+const bcrypt = require('bcryptjs');
+
+const userSchema = new Schema({
   name: {
     type: String,
     required: true,
@@ -8,6 +11,7 @@ const User = mongoose.model('User', {
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -39,5 +43,30 @@ const User = mongoose.model('User', {
     }
   }
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to Login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+  return user;
+};
+
+//hasing plain text pwd before saving
+
+userSchema.pre('save', async function(next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- src/routers/task.js
+++ src/routers/task.js
@@ -38,10 +38,10 @@ router.get('/tasks/:id', async (req, res) => {
 });
 
 router.patch('/tasks/:id', async (req, res) => {
-  const key = Object.keys(req.body);
+  const updates = Object.keys(req.body);
   const allowedToUpdate = ['description', 'completed'];
 
-  const isvalidUpdation = key.every(value => {
+  const isvalidUpdation = updates.every(value => {
     return allowedToUpdate.includes(value);
   });
 
@@ -50,10 +50,14 @@ router.patch('/tasks/:id', async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+    const task = await Task.findById(req.params.id);
+
+    updates.forEach(update => (task[update] = req.body[update]));
+    await task.save();
+    // const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    //   new: true,
+    //   runValidators: true
+    // });
     if (!task) {
       return res.status(404).send('No Task Found');
     }
